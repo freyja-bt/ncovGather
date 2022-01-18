d80470e7a39128a4efe05ce8eb9e9d01e9940dca
FreyjaBrandelTanis <30274062+freyja-bt@users.noreply.github.com>
2022-01-18 13:05:45 -0500

    Initial commit

---
+++ R/readncov.R
@@ -0,0 +1,12 @@
+
+ncov_json <- jsonlite::fromJSON("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?where=%27GEOID%27+LIKE+%27%25%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=")
+
+ncov_data <- ncov_json$features$attributes
+
+ncov_data <- ncov_data|>
+  dplyr::mutate(
+    datetime = lubridate::as_datetime(as.numeric(Last_Update)/1000)
+  )|>
+  dplyr::select(-Last_Update)
+
+write.csv(ncov_data, "data/ncov_series.csv", quote = F, row.names = F, append = T)
